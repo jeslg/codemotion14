f47f5de09ac0f2483a7ed797930edf72df2b94eb
Jesús López-González <jesus@kathars.es>
2014-10-15 16:23:13 +0200

    Added error handling.
    Now, if the word does not exist in the dictionary, a nicer error will
    be shown, with the corresponding 404 error status code.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,16 +5,18 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  var state = Map(
+  val state = Map(
     "code" -> "a collection of laws or rules",
     "emotion" -> "a feeling of any kind")
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def search(word: String) = Action {
+    state.get(word) map (Ok(_)) getOrElse {
+      NotFound("unknown word!")
+    }
   }
 
-  def search(word: String) = Action {
-    Ok(state(word))
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
   }
 
 }
