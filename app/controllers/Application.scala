ad1072d7fff7743f4cd08be4c9ecaeb41d54f669
Jesús López-González <jesus@kathars.es>
2014-10-15 16:09:18 +0200

    Added the minimal service.
    Now, we have a minimal search service for our dictionary. If the user
    introduces a known word, its associated definition is shown.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,16 @@ import play.api.mvc._
 
 object Application extends Controller {
 
+  var state = Map(
+    "code" -> "a collection of laws or rules",
+    "emotion" -> "a feeling of any kind")
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def search(word: String) = Action {
+    Ok(state(word))
   }
 
 }
