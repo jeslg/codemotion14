b3963143331e6a2c736700e5d2e28ff1e46d2363
Jesús López-González <jesus@kathars.es>
2014-10-16 16:26:29 +0200

    Added a regular expression to identify naughty whole words.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -47,7 +47,7 @@ object Application extends Controller {
   // def add(word: String) = WordFilter(word)(parse.text) { request =>
   def add(word: String) = Action.async(parse.text) { request =>
     naughtyList map { list =>
-      list find (request.body contains _) match {
+      list find (request.body.split(" +") contains _) match {
         // FIXME: non-atomic operation => concurrency issues
         case None => {
           state = state + (word -> request.body)
