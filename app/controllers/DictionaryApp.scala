11d2b820fbba1ee00884703815dc69ace541c14d
Jesús López-González <jesus@kathars.es>
2014-10-30 12:11:28 +0100

    Added a minimal search service and Logging action.

--- app/controllers/DictionaryApp.scala
+++ app/controllers/DictionaryApp.scala
@@ -5,6 +5,9 @@ import play.api.cache.Cache
 import play.api.mvc._
 import play.api.Play.current
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 trait DictionaryApp { this: Controller =>
 
   // TODO: Should we move this to "models"?
@@ -26,9 +29,39 @@ trait DictionaryApp { this: Controller =>
     def set(entry: Entry) = Cache.set("dictionary", dictionary + entry)
   }
 
-  def helloDictionary = Action {
-    Ok("Welcome to the CodeMotion14 Dictionary!")
+  case class Logging[A](action: Action[A]) extends Action[A] {
+
+    def apply(request: Request[A]): Future[Result] = {
+      Logger.info(request.toString)
+      action(request)
+    }
+
+    lazy val parser = action.parser
   }
+
+  def helloDictionary = 
+    Logging {
+      Action { request =>
+	Ok("Welcome to the CodeMotion14 Dictionary!")
+      }
+    }
+
+  def search(word: String) = 
+    Logging {
+      Action {
+	Dictionary.get(word).map(Ok(_)).getOrElse {
+	  NotFound(s"could not find '$word'")
+	}
+      }
+    }
+
+  def add(word: String) =
+    Logging {
+      Action(parse.text) { request =>
+	Dictionary.set(word -> request.body)
+	Ok(s"The word '$word' has been added successfully")
+      }
+    }
 }
 
 object DictionaryApp extends Controller with DictionaryApp
