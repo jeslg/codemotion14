91e75a0702eaf5ac154b386dd020c872ef980601
Jesús López-González <jesus@kathars.es>
2014-11-11 12:21:53 +0100

    Update User.scala

--- app/es/scalamad/dictionary/models/User.scala
+++ app/es/scalamad/dictionary/models/User.scala
@@ -11,20 +11,3 @@ case class User(
 
   def nick = s"${name.toLowerCase}_${last.toLowerCase}"
 }
-
-// class CacheUserRepository extends UserRepository {
-
-//   type Users = Map[String, User]
-
-//   private def users = Cache.getOrElse[Users]("users")(Map())
-
-//   def get(nick: String): Option[User] = users get nick
-
-//   def add(user: User): Unit = {
-//     Logger.info(s"Adding '${user.nick}' to user list.")
-//     Cache.set("users", users + (user.nick -> user))
-//   }
-
-//   def reset(users: User*): Unit =
-//     Cache.set("users", users.map(u => u.nick -> u).toMap)
-// }
