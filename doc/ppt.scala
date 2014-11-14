a6606f0e93f69097d8961dc7b11a92574dcdb786
Juan Manuel Serrano <juanmanuel.serrano.hidalgo@gmail.com>
2014-11-14 08:00:16 +0100

    --ppt--

--- doc/ppt.scala
+++ doc/ppt.scala
@@ -1,11 +1,25 @@
+object Slide1{
 
+  val factorial: Int => Int
 
-val factorial: Int => Int
+  val scale: Int => Image => Image
 
-val scale: Int => Image => Image
+  val animation: Function1[Double, Image]
 
-val animation: Function1[Double, Image]
+  val server: Request => State => (Result, State)
 
-val server: Request => State => (Result, State)
+  val … << choose your favourite domain >>
 
-val … << choose your favourite domain >>
+}
+
+object Slide2{
+
+  def factorial(i: Int): Int = ???
+
+  def scale(i: Int)(img1: Image): Image
+
+  def animation: Function1[Double, Image]
+
+  def server(req: Request)(implicit ctx: State): (Result, State)
+
+}
