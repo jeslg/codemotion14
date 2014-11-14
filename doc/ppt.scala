06f40b17af9085ef4215788b976dd267017a57f2
Juan Manuel Serrano <juanmanuel.serrano.hidalgo@gmail.com>
2014-11-14 07:56:34 +0100

    --ppt--

---
+++ doc/ppt.scala
@@ -0,0 +1,11 @@
+
+
+val factorial: Int => Int
+
+val scale: Int => Image => Image
+
+val animation: Function1[Double, Image]
+
+val server: Request => State => (Result, State)
+
+val … << choose your favourite domain >>
