5c781307b06606939da2c3a6cda59ed757350df1
Jesús López-González <jesus@kathars.es>
2014-11-20 16:43:25 +0100

    Added kleisli flatMap and composeK.

--- app/es/scalamad/dictionary/services/effect.scala
+++ app/es/scalamad/dictionary/services/effect.scala
@@ -4,20 +4,26 @@ import es.scalamad.dictionary.models._
 
 sealed trait Effect[A] {
 
-  // def map[B](f: A => B): Effect[B] = ???
-
-  // def flatMap[B](f: A => Effect[B]): Effect[B] = this match {
-  //   case GetEntry(word, next) => { 
-  //     GetEntry(word, (x: String) => next(x) flatMap g)
-  //   }
-  // }
+  def flatMap[B](f: A => Effect[B]): Effect[B] = this match {
+    case GetEntry(word, next) => GetEntry(word, next(_) flatMap f)
+    case SetEntry(entry, next) => SetEntry(entry, next flatMap f)
+    case RemoveEntry(word, next) => RemoveEntry(word, next flatMap f)
+    case ResetEntries(state, next) => ResetEntries(state, next flatMap f)
+    case GetUser(nick, next) => GetUser(nick, next(_) flatMap f)
+    case SetUser(user, next) => SetUser(user, next flatMap f)
+    case RemoveUser(nick, next) => RemoveUser(nick, next flatMap f)
+    case ResetUsers(state, next) => ResetUsers(state, next flatMap f)
+    case CanRead(user, next) => CanRead(user, next(_) flatMap f)
+    case CanWrite(user, next) => CanWrite(user, next(_) flatMap f)
+    case Return(value) => f(value)
+  }
 }
 
 object Effect {
 
-  // def composeK[A, B, C](
-  //   g: B => Effect[C],
-  //   f: A => Effect[B]): A => Effect[C] = f(_) flatMap g
+  def composeK[A, B, C](
+    g: B => Effect[C],
+    f: A => Effect[B]): A => Effect[C] = f(_) flatMap g
 }
 
 case class Return[A](value: A) extends Effect[A]
